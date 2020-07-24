4e29ce61d639fe27434883f25cbc72ac69196556
bastianklwenon <bgallardochavarra@gmail.com>
2020-07-24 13:34:00 -0400

    docs agregado

---
+++ docs/precache-manifest.6535df6c32ff8b2608201fadbfe0706c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "708b31bcbcd12cf629bdb73018caa388",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f17bdf03ace841ff121a",
+    "url": "/static/css/main.07c26d3e.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17bdf03ace841ff121a",
+    "url": "/static/js/main.fdc1c233.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
